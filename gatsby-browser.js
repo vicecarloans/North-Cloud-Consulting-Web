21f2f61b1713b2cc079a77084a9f67ef6d1416b8
vicecarloans <huyst1997@icloud.com>
2020-09-18 17:40:58 -0400

    Update Gatsby-Browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,11 @@ export const shouldUpdateScroll = ({
     routerProps: { location },
     getSavedScrollPosition,
 }) => {
-
-    window.scrollTo({top: 0, behavior: "smooth"})
-
-    return false
+    const { pathname } = location;
+    // list of routes for the scroll-to-top-hook
+    const scrollToTopRoutes = [`/`];
+    if (scrollToTopRoutes.indexOf(pathname) !== -1) {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    return false;
 };
